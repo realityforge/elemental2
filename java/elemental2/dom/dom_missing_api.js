36f6cdda539e5cbf508966de96309b6fda70a891
Colin Alworth <colin@colinalworth.com>
2018-07-14 20:28:28 -0500

    Added missing Document.activeElement, standardized but not moved in closure externs
    Fixes #40

--- java/elemental2/dom/dom_missing_api.js
+++ java/elemental2/dom/dom_missing_api.js
@@ -436,6 +436,15 @@ Window.prototype.devicePixelRatio;
  */
 Window.prototype.getSelection = function() {};
 
+
+/**
+ * Defined in ie_dom.js
+ *
+ * @type {!Element}
+ * @see http://msdn.microsoft.com/en-us/library/ms533065(VS.85).aspx
+ */
+Document.prototype.activeElement;
+
 /**
  * @type {!Console}
  * @suppress {duplicate}
